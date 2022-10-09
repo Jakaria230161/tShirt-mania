0071566c90906cd2c265d12273e15c9e7076a555
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-09 19:25:37 +0600

    setup router and create some components

---
+++ src/---steps.js
@@ -0,0 +1,11 @@
+/*
+BASIC ROUTER SET-UP:
+1,npm install router dom
+2,create a router using createBrowserRouter
+3,add RouterProvider and set router prop
+4,create a lot of components
+5,create a main layout with some common parts and one changing part based on the router.
+6, set main layout at the root of the route
+7,set children routes
+8,set links on the header components
+*/
--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Orders from './Components/Orders/Orders';
+import Main from './layouts/Main';
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        }
+      ]
+    }
+  ]);
   return (
     <div className="App">
-        
+      <RouterProvider router={router}></RouterProvider>  
     </div>
   );
 }
