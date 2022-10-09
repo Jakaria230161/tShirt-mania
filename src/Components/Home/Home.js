3832f64ffcb68bd4e216aa460567fcb1f63d0e23
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-09 20:08:27 +0600

    show tShirt on the home menu

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import './Home.css';
+
 
 const Home = () => {
+    const tshirts = useLoaderData();
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='home-container'>
+            <div className='t-shirt-container'>
+            {
+                tshirts.map(tshirt => <TShirt
+                    key={tshirt.id}
+                    tshirt={tshirt}
+                ></TShirt>)    
+            }
+            </div>
+
+            <div className='cart-container'>
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
